a4eb1b6e4482f109fd8ad0c449ca10e4d48b4aaf
Al366io <alessio.nannipieri0@gmail.com>
2022-09-24 16:34:13 +0200

    first commit

---
+++ EloquentExercises.js
@@ -0,0 +1,67 @@
+/*
+    Ex n.1
+    Looping a triangle
+    Write a loop that makes seven calls to console.log to output the following triangle:
+
+    #
+    ##
+    ###
+    ####
+    #####
+    ######
+    #######
+    
+    It may be useful to know that you can find the length of a string by writing .length after it.
+*/
+
+function loopingTriangle(num) {
+    // SOLVE HERE
+    let string = '#';
+    for (let i=0; i<num; i++) {
+        console.log(string);
+        string.append('#');
+    }
+};
+
+// tester. leave untouched.
+console.log(loopingTriangle(7))
+
+/*
+    FizzBuzz
+    Write a program that uses console.log to print all the numbers from 1 to 100, with two exceptions.
+    For numbers divisible by 3, print "Fizz" instead of the number, and for numbers divisible by 5 
+    (and not 3), print "Buzz" instead.
+
+    When you have that working, modify your program to print "FizzBuzz" for numbers that are 
+    divisible by both 3 and 5 (and still print "Fizz" or "Buzz" for numbers divisible by only
+    one of those).
+*/
+
+function fizzBuzz() {
+    // SOLVE HERE
+}
+
+/*
+    Chessboard
+    Write a program that creates a string that represents an 8×8 grid, using newline characters
+    to separate lines. At each position of the grid there is either a space or a "#" character. 
+    The characters should form a chessboard.
+
+    Passing this string to console.log should show something like this:
+
+    # # # #
+    # # # # 
+    # # # #
+    # # # # 
+    # # # #
+    # # # # 
+    # # # #
+    # # # #
+    When you have a program that generates this pattern, define a binding size = 8 
+    and change the program so that it works for any size, outputting a grid of the 
+    given width and height. 
+*/
+
+function chessboard() {
+    //SOLVE HERE
+}
