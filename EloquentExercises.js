1fe21cf2cd228bfd750fd8b0f71b4dbb9871a965
Al366io <alessio.nannipieri0@gmail.com>
2022-10-04 08:49:38 +0200

    added Exs

--- EloquentExercises.js
+++ EloquentExercises.js
@@ -158,3 +158,71 @@ JSON.stringify(sumOfRange(10,1,-2))===JSON.stringify(arrBuffsor) ? console.log('
 function reverseArrayInPlace(arr) {
   //SOLVE HERE
 }
+
+console.log('\n-------- TESTING reverseArrayInPlace --------\n');
+let array=[1,2,3,4,5,6];
+let arrayReversed=[6,5,4,3,2,1];
+JSON.stringify(reverseArrayInPlace(array))===JSON.stringify(arrayReversed) ? console.log('ok') : console.log('Error');
+
+/**
+  A LIST
+
+  Objects, as generic blobs of values, can be used to build all sorts of data structures. 
+  A common data structure is the list (not to be confused with array). A list is a nested set of objects, 
+  with the first object holding a reference to the second, the second to the third, and so on.
+
+  let list = {
+    value: 1,
+    rest: {
+      value: 2,
+      rest: {
+        value: 3,
+        rest: null
+      }
+    }
+  };
+  The resulting objects form a chain. (Linked list)
+
+  A nice thing about lists is that they can share parts of their structure. 
+  For example, if I create two new values {value: 0, rest: list} and {value: -1, rest: list} 
+  (with list referring to the binding defined earlier), they are both independent lists,
+  but they share the structure that makes up their last three elements. 
+  The original list is also still a valid three-element list.
+
+  Write a function arrayToList that builds up a list structure like the one shown when given [1, 2, 3] as argument. 
+  Also write a listToArray function that produces an array from a list. Then add a helper function prepend, 
+  which takes an element and a list and creates a new list that adds the element to the front of the input list, 
+  and nth, which takes a list and a number and returns the element at the given position in the list 
+  (with zero referring to the first element) or undefined when there is no such element.
+
+  If you haven’t already, also write a recursive version of nth. 
+
+**/
+
+function aList() {
+
+}
+
+console.log('\n-------- TESTING aList --------\n');
+
+/* 
+
+  DEEP COMPARISON
+  The == operator compares objects by identity. But sometimes you’d prefer to compare the values of their actual properties.
+
+  Write a function deepEqual that takes two values and returns true only if they are the same value or are objects 
+  with the same properties, where the values of the properties are equal when compared with a recursive call to deepEqual.
+
+  To find out whether values should be compared directly (use the === operator for that) or have their properties compared,
+  you can use the typeof operator. If it produces "object" for both values, you should do a deep comparison. 
+  But you have to take one silly exception into account: because of a historical accident, typeof null also produces "object".
+
+  The Object.keys function will be useful when you need to go over the properties of objects to compare them.
+
+*/
+
+function deepComparison() {
+
+}
+
+console.log('\n-------- TESTING deepComparison --------\n');
